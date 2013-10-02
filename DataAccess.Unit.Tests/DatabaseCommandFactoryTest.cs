6f044b4eb53aee5140ec6145291720032a0f6640
David Miranda <davidmiranda11@gmail.com>
2013-10-02 23:47:26 +0100

    removed duplicate test

--- DataAccess.Unit.Tests/DatabaseCommandFactoryTest.cs
+++ DataAccess.Unit.Tests/DatabaseCommandFactoryTest.cs
@@ -10,60 +10,70 @@ namespace DataAccess.Unit.Tests
     [TestFixture]
     public class DatabaseCommandFactoryTest
     {
-        private Mock<IDatabaseCommandInstaceProvider> _connectionManager;
         private DatabaseCommandFactory _databaseCommandFactory;
-        private DataQuery _dataQuery;
-        private Mock<IDbCommand> _command;
+        private Mock<IDatabaseCommandInstaceProvider> _connectionManager;
+        private Mock<IDbCommand> _dbCommand;
         private Mock<IDbDataParameter> _dataParameter;
-        private Mock<IDataParameterCollection> _dataParameterCollection;
-        private Mock<ITransactionManager> _transactionManager;
+        private Mock<IDataParameterCollection> _dataParmeterCollection;
 
         [SetUp]
         public void SetUp()
         {
             _connectionManager = new Mock<IDatabaseCommandInstaceProvider>();
-            _transactionManager = new Mock<ITransactionManager>();
-            _databaseCommandFactory = new DatabaseCommandFactory(_connectionManager.Object);
-            _dataQuery = new DataQuery();
-            _command = new Mock<IDbCommand>();
+            _dbCommand = new Mock<IDbCommand>();
             _dataParameter = new Mock<IDbDataParameter>();
-            _dataParameterCollection = new Mock<IDataParameterCollection>();
-            _command.Setup(x => x.Parameters).Returns(_dataParameterCollection.Object);
-            _command.Setup(x => x.CreateParameter()).Returns(_dataParameter.Object);
-            _connectionManager.Setup(x => x.CreateCommandForCurrentConnection()).Returns(_command.Object);
-            
+            _dataParmeterCollection = new Mock<IDataParameterCollection>();
+            _databaseCommandFactory = new DatabaseCommandFactory(_connectionManager.Object);
+
+            _connectionManager.Setup(x => x.CreateCommandForCurrentConnection()).Returns(_dbCommand.Object);
+            _dbCommand.Setup(x => x.CreateParameter()).Returns(_dataParameter.Object);
+            _dbCommand.Setup(x => x.Parameters).Returns(_dataParmeterCollection.Object);
         }
 
         [Test]
-        public void ShouldInitialiseCommand()
+        public void ShouldObtainAFreshCommandForTheCurrentConnection()
         {
-            _dataQuery.WithStoredProc("GetShizzle");
-
             //When
-            _databaseCommandFactory.CreateCommandFor(_dataQuery);
-
-            //Then
-            _connectionManager.Verify(x => x.CreateCommandForCurrentConnection(), Times.Once());
-            _command.VerifySet(x => x.CommandText = "GetShizzle");
-            _command.VerifySet(x => x.CommandType = CommandType.StoredProcedure);
-        }
-
-        [Test]
-        public void ShouldAddAllParametersToCommand()
-        {
-            //Given
-            _dataQuery.WithParam("@FirstName", "David").WithParam("@Surname", "Miranda");
-
-            //When
-            _databaseCommandFactory.CreateCommandFor(_dataQuery);
+            var command = _databaseCommandFactory.CreateCommandFor(new DataQuery());
     
             //Then
-            _command.Verify(x => x.CreateParameter(), Times.Exactly(2));
-            _dataParameter.VerifySet(x => x.ParameterName = "@FirstName");
-            _dataParameter.VerifySet(x => x.Value = "David");
-            _dataParameter.VerifySet(x => x.ParameterName = "@Surname");
-            _dataParameter.VerifySet(x => x.Value = "Miranda");
-            _dataParameterCollection.Verify(x => x.Add(_dataParameter.Object), Times.Exactly(2));
+            _connectionManager.Verify(x => x.CreateCommandForCurrentConnection(), Times.Once());
+            Assert.That(command, Is.EqualTo(_dbCommand.Object));
+        }
+
+        [Test]
+        public void ShouldInitaliseCommandProperties()
+        {
+            //Given
+            var dataQuery = new DataQuery().WithStoredProc("Get_Shizzle");
+
+            //When
+            _databaseCommandFactory.CreateCommandFor(dataQuery);
+
+            //Then
+            _dbCommand.VerifySet(x => x.CommandText = "Get_Shizzle");
+            _dbCommand.VerifySet(x => x.CommandType = CommandType.StoredProcedure);
+        }
+
+        [Test]
+        public void ShouldInitialiseCommandDataParameters()
+        {
+            //Given
+            var dataQuery = new DataQuery()
+                .WithParam("Title", "Mr")
+                .WithParam("FirstName", "David")
+                .WithParam("Surname", "Miranda");
+
+            //When
+            _databaseCommandFactory.CreateCommandFor(dataQuery);
+
+            //Then
+            _dataParameter.VerifySet(x => x.ParameterName = "Title", Times.Once());
+            _dataParameter.VerifySet(x => x.Value =  "Mr", Times.Once());
+            _dataParameter.VerifySet(x => x.ParameterName = "FirstName", Times.Once());
+            _dataParameter.VerifySet(x => x.Value =  "David", Times.Once());
+            _dataParameter.VerifySet(x => x.ParameterName = "Surname", Times.Once());
+            _dataParameter.VerifySet(x => x.Value = "Miranda", Times.Once());
         }
     }
 }
